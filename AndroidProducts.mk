044346b4b10478eaeb323f9eaed18c922239f64a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-23 09:04:45 +0100

    Y7: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: tp-link Model: Neffos_Y7 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Y7.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Y7-user \
+    omni_Y7-userdebug \
+    omni_Y7-eng
